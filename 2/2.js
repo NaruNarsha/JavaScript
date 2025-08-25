92cdc57f175132392cd1d5bbfdf59c36c04ba6dc
문경림 <naru@takeone.co.kr>
2025-08-25 12:36:37 +0900

    update 2

---
+++ 2/2.js
@@ -0,0 +1,28 @@
+
+/*
+
+ajax = 서버에서 데이터 가져오기
+
+html 상단부 적는 것 => !+엔터하면 쫙 적힘
+
+컨트롤+쉬프트+i -> 콘솔로그 확인가능
+
+공부할때 -> developer.mozilla.org 사이트 방문하기
+
+효율적인 코드 defer는 파싱(코드읽어내려가는 과정)중 동시에 다운(fetching)받아 실행(execute)해서 좋음.
+
+ex. <script defer src="a.js"></script> (헤드안임)
+
+순수 자바스크립트로만 코딩시 'use strict'; 상단부에 적기
+
+-----------------------------------------------------------
+
+
+2022년 새로운 자바스크립트 마스터리 (ES6+ 최신문법) 강의가 런칭 되었어요.  
+유튜브보다 체계적으로 꼼꼼하게 기본&고급 내용 전부 배워보고 싶으시다면 
+    → https://academy.dream-coding.com/courses/javascript
+    
+📒 강의 노트 & 자주 나오는 질문: https://github.com/dream-ellie/learn-javascript
+
+
+*/
